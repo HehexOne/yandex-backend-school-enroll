9a7ea32bfabf571b1902a01f5454689c8e7291dd
Alexander Alpatov <werty.mn2@yandex.ru>
2022-06-17 19:28:26 +0300

    uniquness check

--- main.py
+++ main.py
@@ -18,18 +18,16 @@ def is_valid_date(mts):
 
 
 def is_valid_price(sui: ShopUnitImport):
-    if not sui.price and sui.type == ShopUnitType.CATEGORY:
-        return True
-    elif sui.type == ShopUnitType.OFFER and sui.price and (0 <= sui.price <= 9223372036854775807):
+    if (not sui.price and sui.type == ShopUnitType.CATEGORY) or\
+            (sui.type == ShopUnitType.OFFER and sui.price and (0 <= sui.price <= 9223372036854775807)):
         return True
     else:
         return False
 
 
 def is_valid_shop_unit_import_request(suir: ShopUnitImportRequest):
-    if not all(map(is_valid_price, suir.items)):
-        return False
-    elif not is_valid_date(suir.updateDate):
+    if (not all(map(is_valid_price, suir.items))) or (not is_valid_date(suir.updateDate)) or\
+            len(suir.items) != len(set(map(lambda item: item.id, suir.items))):
         return False
     else:
         return True
