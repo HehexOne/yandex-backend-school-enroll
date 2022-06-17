0c6766af68249daf23ce06b1729bdc1a4f9d8eef
Alexander Alpatov <werty.mn2@yandex.ru>
2022-06-17 19:01:49 +0300

    imports

--- main.py
+++ main.py
@@ -1,40 +1,74 @@
-import json
-
+from datetime import datetime
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import PlainTextResponse
+from schema import *
 
-app = FastAPI()
+validation_error = Error(code=400, message="Validation Failed").json()
+todo_error = Error(code=500, message="TODO").json()
+ok_error = Error(code=200, message="OK!").json()
+
+
+def is_valid_date(mts):
+    try:
+        datetime.strptime(mts, '%Y-%m-%dT%H:%M:%S.%f%z')
+        return True
+    except ValueError:
+        return False
+
+
+def is_valid_price(sui: ShopUnitImport):
+    if not sui.price and sui.type == ShopUnitType.CATEGORY:
+        return True
+    elif sui.type == ShopUnitType.OFFER and sui.price and (-9223372036854775808 <= sui.price <= 9223372036854775807):
+        return True
+    else:
+        return False
+
+
+def is_valid_shop_unit_import_request(suir: ShopUnitImportRequest):
+    if not all(map(is_valid_price, suir.items)):
+        return False
+    elif not is_valid_date(suir.updateDate):
+        return False
+    else:
+        return True
+
+
+app = FastAPI(title="Mega Market Open API",
+              version="1.0",
+              openapi_version="3.0.0",
+              description="Вступительное задание в Летнюю Школу Бэкенд Разработки Яндекса 2022")
 
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
-    return PlainTextResponse(json.dumps({
-        "code": 400,
-        "message": "Validation Failed"
-    }), status_code=400)
+    return PlainTextResponse(validation_error, status_code=400)
 
 
 @app.post("/imports")
-def imports():
-    return {"message": "TODO"}
+async def imports(shop_unit_import_request: ShopUnitImportRequest):
+    if is_valid_shop_unit_import_request(shop_unit_import_request):
+        return PlainTextResponse(ok_error, status_code=200)
+    else:
+        return PlainTextResponse(validation_error, status_code=400)
 
 
 @app.delete("/delete/{id}")
-def delete_by_id(id: int):
-    return {"message": "TODO"}
+async def delete_by_id(id: int):
+    return PlainTextResponse(todo_error, status_code=500)
 
 
 @app.delete("/nodes/{id}")
-def nodes(id: int):
-    return {"message": "TODO"}
+async def nodes(id: int):
+    return PlainTextResponse(todo_error, status_code=500)
 
 
 @app.get("/sales")
-def sales():
-    return {"message": "TODO"}
+async def sales():
+    return PlainTextResponse(todo_error, status_code=500)
 
 
 @app.get("/node/{id}/statistic")
-def node_statistic(id: int):
-    return {"message": "TODO"}
+async def node_statistic(id: int):
+    return PlainTextResponse(todo_error, status_code=500)
---
+++ schema.py
@@ -0,0 +1,52 @@
+from pydantic import BaseModel
+from enum import Enum
+from typing import Optional, List
+
+
+# TODO Type checking in main file (datetime (date) and int64 (price))
+
+
+class ShopUnitType(str, Enum):
+    OFFER = "OFFER"
+    CATEGORY = "CATEGORY"
+
+
+class ShopUnit(BaseModel):
+    id: str
+    name: str
+    date: str
+    parentId: Optional[str] = None
+    type: ShopUnitType
+    price: Optional[int] = None
+    children: List = []
+
+
+class ShopUnitImport(BaseModel):
+    id: str
+    name: str
+    parentId: Optional[str] = None
+    type: ShopUnitType
+    price: Optional[int] = None
+
+
+class ShopUnitImportRequest(BaseModel):
+    items: List[ShopUnitImport]
+    updateDate: str
+
+
+class ShopUnitStatisticUnit(BaseModel):
+    id: str
+    name: str
+    parentId: Optional[str] = None
+    type: ShopUnitType
+    price: Optional[int] = None
+    date: str
+
+
+class ShopUnitStatisticResponse(BaseModel):
+    items: List[ShopUnitStatisticUnit] = []
+
+
+class Error(BaseModel):
+    code: int
+    message: str
